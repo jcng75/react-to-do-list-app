5be3b51df7a7a2c22bc26127debe5ab112ba2026
Justin <justinchunng@gmail.com>
2025-03-04 01:27:31 -0500

    Updated app.jsx base

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import "./styles.css"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form className="new-item-form">
+      <div className="form-row">
+      <label htmlFor="itemName">New Item</label>
+      <input id="itemName" type="text" />
+      <button className="btn">Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </form>
+    <h1 className="header"> Todo List</h1>
+    <ul className="todo-list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      </ul>
     </>
-  )
+  ) 
 }
-
-export default App
